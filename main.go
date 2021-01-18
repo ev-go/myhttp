be5726ce0ca4de8882b4b39fd572f1373a01d306
ev <dmitry_dtv@mail.ru>
2021-01-18 10:42:18 +0500

    210118

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+type Message struct {
+	Name string
+	Body string
+	Time int64
+}
+
 func main() {
+
+	m := Message{"Alice", "Hello", 1294706395881547000}
+	b, err := json.Marshal(m)
+
 	var Log string
 	fmt.Scanf("%s\n", &Log)
 	// fmt.Println(Log)
@@ -19,7 +30,8 @@ func main() {
 	// }
 	router.HandleFunc("/main", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, Log)
-		fmt.Fprint(w, r)
+		fmt.Fprint(w, err)
+		fmt.Fprint(w, b)
 	})
 
 	http.ListenAndServe(":3000", router)
