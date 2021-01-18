6da85c3dff9d5a4b9e8281db21fb6f7827c4f957
Ev <Ev>
2021-01-18 19:29:28 +0500

    210118_2

--- main.go
+++ main.go
@@ -17,7 +17,9 @@ type Message struct {
 func main() {
 
 	m := Message{"Alice", "Hello", 1294706395881547000}
+	fmt.Println(m)
 	b, err := json.Marshal(m)
+	fmt.Println(b)
 
 	var Log string
 	fmt.Scanf("%s\n", &Log)
@@ -32,6 +34,11 @@ func main() {
 		fmt.Fprint(w, Log)
 		fmt.Fprint(w, err)
 		fmt.Fprint(w, b)
+	})
+
+	router.HandleFunc("/2nd", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Server", "A Go Web Server")
+		w.WriteHeader(200)
 	})
 
 	http.ListenAndServe(":3000", router)
